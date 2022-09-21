b6ad10b991bd546fa2fac1e546bcff462e4f54e8
Alex Moriarty <25207558+a-ws-m@users.noreply.github.com>
2022-09-21 18:18:20 +0100

    Better IO routine and simple experiment setup

--- camcann/data/io.py
+++ camcann/data/io.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 from rdkit.Chem import MolFromSmiles
 from sklearn.model_selection import KFold
-from spektral.data import Dataset, Graph
+from spektral.data import Dataset, DisjointLoader, Graph
 from spektral.layers import GCNConv
 from spektral.transforms import LayerPreprocess
 
@@ -16,12 +16,14 @@ from .featurise.graph import MolNodeFeaturizer, mols_to_graph
 DATASET_FOLDER = Path(__file__).parent / "datasets"
 RANDOM_SEED = 2021
 
+
 class Datasets(Enum):
     """Available datasets."""
 
     QIN = DATASET_FOLDER / "qin-data.csv"
     NIST_ANIONICS = DATASET_FOLDER / "nist-anionics.csv"
     QIN_AND_NIST_ANIONICS = DATASET_FOLDER / "merged-data.csv"
+
 
 class QinDatasets(Enum):
     """Qin datasets split by test and train subsets."""
@@ -36,51 +38,99 @@ class DataReader:
     def __init__(self, dataset: Datasets) -> None:
         """Read data from disk."""
         self.df = pd.read_csv(dataset.value, header=0)
-    
+
     @property
-    def cv_indexes(self, num_folds: int = 10, random_seed: int = RANDOM_SEED) -> List[Tuple[List[int], List[int]]]:
+    def cv_indexes(
+        self, num_folds: int = 10, random_seed: int = RANDOM_SEED
+    ) -> List[Tuple[List[int], List[int]]]:
         """Get the list of indexes in each fold of a K-fold cross-validation split.
-        
+
         Args:
             num_folds: The number of folds.
             random_seed: The random state to use when shuffling the data.
-        
+
         Returns:
             A list of ``(train_indexes, test_indexes)`` for each fold.
-        
+
         """
         kf = KFold(n_splits=num_folds, shuffle=True, random_state=random_seed)
         return list(kf.split(self.df))
-    
+
     @property
-    def train_test_data(self, fold: int, num_folds: int = 10, random_seed: int = RANDOM_SEED) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def train_test_data(
+        self, fold: int, num_folds: int = 10, random_seed: int = RANDOM_SEED
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """Get train and test slices for a given fold."""
-        train_idxs, test_idxs = self.cv_indexes(num_folds=num_folds, random_seed=random_seed)[fold]
+        train_idxs, test_idxs = self.cv_indexes(
+            num_folds=num_folds, random_seed=random_seed
+        )[fold]
         return self.df.iloc[train_idxs], self.df.iloc[test_idxs]
 
-class QinGraphData(Dataset):
+
+class QinGraphData:
     """Handle reading Qin datasets from file and splitting into train and test subsets."""
 
-    def __init__(self, dataset: QinDatasets, read_train: bool=True, mol_featuriser: MolNodeFeaturizer = MolNodeFeaturizer()) -> None:
+    def __init__(
+        self,
+        dataset: QinDatasets,
+        mol_featuriser: MolNodeFeaturizer = MolNodeFeaturizer(),
+    ) -> None:
         """Load data.
-        
+
         Args:
             dataset: Which dataset to load.
             read_train: Whether to read train data. :meth:`read` will load test data if this is ``False``.
             mol_featuriser: The molecular featuriser to use. This is important for consistency with featurising, e.g. one hot encoding.
-        
+
         """
-        df = pd.read_csv(dataset.value, header=0, index_col=0)
-        df.Molecules = [MolFromSmiles(smiles) for smiles in df.smiles]
-        df.Graphs = mols_to_graph(df.Molecules, mol_featuriser, df.exp)
-        self.graphs = list(df.Graphs)
+        self.df = pd.read_csv(dataset.value, header=0, index_col=0)
+        self.df.Molecules = [MolFromSmiles(smiles) for smiles in self.df.smiles]
+        self.df.Graphs = mols_to_graph(self.df.Molecules, mol_featuriser, self.df.exp)
+        self.graphs = list(self.df.Graphs)
         self.apply(LayerPreprocess(GCNConv))
 
-        self.test_idxs, _ = np.where(df.traintest=="test")
-        self.train_idxs, _ = np.where(df.traintest=="train")
+        self.test_idxs, _ = np.where(self.df.traintest == "test")
+        self.train_idxs, _ = np.where(self.df.traintest == "train")
 
-        self.read_train: bool = read_train
+    class Subset(Dataset):
+        """Handle graph data subsets."""
 
-    def read(self) -> List[Graph]:
-        """Return the graphs for the selected dataset."""
-        return [self.graphs[i] for i in (self.train_idxs if self.read_train else self.test_idxs)]
+        def __init__(self, graphs: List[Graph], transforms=None):
+            super().__init__(transforms)
+            self.graphs = graphs
+
+        def read(self) -> List[Graph]:
+            """Return the graphs for this subset."""
+            return self.graphs
+
+    @property
+    def train_dataset(self):
+        """Get the training dataset."""
+        train_graphs = [self.graphs[i] for i in self.train_idxs]
+        return self.Subset(self.train_graphs)
+
+    @property
+    def test_dataset(self):
+        """Get the test dataset."""
+        test_graphs = [self.graphs[i] for i in self.test_idxs]
+        return self.Subset(self.test_graphs)
+
+    @property
+    def all_dataset(self):
+        """Get the full dataset."""
+        return self.Subset(self.graphs)
+
+    @property
+    def train_loader(self):
+        """Get the training data loader."""
+        return DisjointLoader(self.train_dataset)
+
+    @property
+    def test_loader(self):
+        """Get the test data loader."""
+        return DisjointLoader(self.test_dataset)
+
+    @property
+    def all_loader(self):
+        """Get the full data loader."""
+        return DisjointLoader(self.all_dataset, shuffle=False)
--- camcann/lab.py
+++ camcann/lab.py
@@ -1,23 +1,96 @@
 """Test the performance of models on the Qin data."""
+from datetime import datetime
 from enum import Enum
 from pathlib import Path
+from typing import Type
 
+import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+from tensorflow.keras.metrics import (
+    MeanAbsoluteError,
+    RootMeanSquaredError,
+)
 from tensorflow.keras.models import Model
 
-from .data.io import QinGraphData, QinDatasets
-from .gnn import QinGNN, CoarseGNN
+from .data.io import QinDatasets, QinGraphData
+from .gnn import CoarseGNN, QinGNN
+
 
 class Experiment:
     """Train a model on the Qin data, then report the results."""
-    def __init__(self, model: Model, name: str) -> None:
-        self.model = model
-        self.name = name
-    
-    class Paths(Enum):
-        """Contains the paths that the model files are stored in."""
-        model: Path(self.name)
 
+    def __init__(
+        self, model: Type[Model], dataset: QinDatasets, results_path: Path
+    ) -> None:
+        """Initialize the model and the datasets."""
+        self.model = model()
+        self.model.compile(
+            optimizer="adam",
+            loss="mse",
+            metrics=[RootMeanSquaredError(), MeanAbsoluteError()],
+        )
+        self.graph_data = QinGraphData(dataset)
 
-    def train(self):
+        self.results_path = results_path
+        self.model_path = results_path / "model"
+        self.predict_path = results_path / "predictions.csv"
+        self.tb_dir = results_path / "logs"
+        self.metrics_path = results_path / "metrics.csv"
+
+    @property
+    def tb_run_dir(self) -> Path:
+        """Get a new tensorboard log directory for a current run."""
+        return self.tb_dir / str(datetime.now().strftime("%Y.%m.%d.%H.%M.%S"))
+
+    def train(self, epochs: int):
         """Train the model, reporting data via tensorboard."""
-        ...
+        loader = self.graph_data.train_loader
+
+        callbacks = []
+        # if self.patience:
+        #     es_callback = EarlyStopping(
+        #         min_delta=self.min_delta,
+        #         patience=self.patience,
+        #         restore_best_weights=True,
+        #     )
+        #     callbacks.append(es_callback)
+        callbacks.append(TensorBoard(log_dir=self.tb_run_dir))
+
+        self.model.fit(
+            loader.load(),
+            steps_per_epoch=loader.steps_per_epoch,
+            epochs=epochs,
+            callbacks=callbacks,
+        )
+
+    def _make_pred_df(self, predictions):
+        """Make a DataFrame of predicted CMCs."""
+        return pd.DataFrame(
+            {
+                "smiles": self.graph_data.df.smiles,
+                "exp": self.graph_data.df.exp,
+                "qin": self.graph_data.df.pred,
+                "pred": predictions,
+                "traintest": self.graph_data.df.traintest,
+            }
+        )
+
+    def test(self):
+        """Get test metrics and report predictions for all data."""
+        loader = self.graph_data.test_loader
+
+        metrics = self.model.evaluate(
+            loader.load(), steps_per_epoch=loader.steps_per_epoch, return_dict=True
+        )
+        pd.DataFrame(metrics).to_csv(self.metrics_path)
+
+        all_loader = self.graph_data.all_loader
+        predictions = self.model.predict(
+            all_loader.load(), steps_per_epoch=all_loader.steps_per_epoch
+        )
+        self._make_pred_df(predictions).to_csv(self.predict_path)
+
+if __name__ == "__main__":
+    exp = Experiment(QinGNN, QinDatasets.QIN_NONIONICS_RESULTS, results_path=Path(".") / "test_model")
+    exp.train(500)
+    exp.test()
