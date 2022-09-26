93568256b5df2bf20f485333745d446403466463
Alex Moriarty <25207558+a-ws-m@users.noreply.github.com>
2022-09-26 22:05:43 +0100

    Switch back to GCN

--- camcann/gnn.py
+++ camcann/gnn.py
@@ -1,6 +1,6 @@
 from typing import List
 from spektral.layers import GCNConv, GlobalAvgPool, LaPool, TAGConv
-from spektral.layers import ops
+from spektral.layers.ops import normalize_A
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Model
 
@@ -41,13 +41,13 @@ class CoarseGNN(Model):
     def __init__(self) -> None:
         """Initialize layers."""
         super().__init__()
-        self.tag_1 = TAGConv(256, K=2, activation="relu")
-        self.tag_2 = TAGConv(256, K=2, activation="relu")
+        self.gcn_1 = GCNConv(256, activation="relu")
+        self.gcn_2 = GCNConv(256, activation="relu")
 
         self.la_pool = LaPool()
 
-        self.tag_3 = TAGConv(256, K=2, activation="relu")
-        self.tag_4 = TAGConv(256, K=2, activation="relu")
+        self.gcn_3 = GCNConv(256, activation="relu")
+        self.gcn_4 = GCNConv(256, activation="relu")
 
         self.avg_pool = GlobalAvgPool()
 
@@ -56,21 +56,23 @@ class CoarseGNN(Model):
 
         self.out = Dense(1)
 
-        self.full_graph_layers = [self.tag_1, self.tag_2]
-        self.pooled_graph_layers = [self.tag_3, self.tag_4]
+        self.full_graph_layers = [self.gcn_1, self.gcn_2]
+        self.pooled_graph_layers = [self.gcn_3, self.gcn_4]
 
         self.readout_layers = [self.readout_1, self.readout_2, self.out]
 
     def call(self, inputs, training=None, mask=None):
         """Call the model."""
         x, a, _ = inputs
+        norm_a = normalize_A(a)
         for layer in self.full_graph_layers:
-            x = layer((x, a))
+            x = layer((x, norm_a))
 
         x, a = self.la_pool((x, a))
+        norm_a = normalize_A(a)
 
         for layer in self.pooled_graph_layers:
-            x = layer((x, a))
+            x = layer((x, norm_a))
 
         x = self.avg_pool(x)
 
